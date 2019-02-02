9632625ec57e591016c8ca1c8eb3b751e31e6f90
dinosmajovic <dino.smajovic@isatis.com>
2019-02-02 21:09:56 +0100

    :sparkles: Added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AboutPage from './Containers/AboutPage';
+import ProjectsPage from './Containers/ProjectsPage';
+import SkillsPage from './Containers/SkillsPage';
+import ContactPage from './Containers/ContactPage';
+import NotFoundPage from './Containers/NotFoundPage';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Personal Website
-      </div>
+      <Switch>
+        <Route exact path="/" component={AboutPage} />
+        <Route exact path="/projects" component={ProjectsPage} />
+        <Route exact path="/skills" component={SkillsPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
     );
   }
 }
